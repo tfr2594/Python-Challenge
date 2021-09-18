c5adfb473984df1ad66ee3bacfc25dbe642042cc
tfr2594 <tfr2594@gmail.com>
2021-09-18 18:24:17 -0400

    final update for hw

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -1,1 +1,78 @@
-print(f'hello!')
+#check video 2 of python to figure out where to start
+import os
+import csv
+
+budget_data_csv = os.path.join("Resources", "budget_data.csv")
+
+# Objective 3: Create the lists to store data. 
+
+profit = []
+monthly_changes = []
+date = []
+
+# set variables 
+ 
+initial_profit = 0
+
+# Open the CSV using the set path PyBankcsv
+
+with open(budget_data_csv, encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    # Read the header row first (skip this part if there is no header)
+    csv_header = next(csv_file)
+    print(f"Header: {csv_header}")
+
+    # Read through each row of data after the header
+    #for row in csv_reader: check the candy segment in python 2
+    #row is the variable that goes line by line down the data putting a [0] or [1] gives youe one of the columns
+    # and if i append each column i can get the totals!
+    
+    for row in csv_reader:    
+
+      # Will need it when collecting the greatest increase and decrease in profits
+      date.append(row[0])
+
+      #change the row to an int type for math functions to work
+      profit.append(int(row[1]))
+      
+      ##to find change = current - previous/inital profit (revist)
+      final_profit = int(row[1])
+      monthly_change_profits = final_profit - initial_profit
+      initial_profit = final_profit
+
+      #where to keep the changes between each month on the empty list above
+      monthly_changes.append(monthly_change_profits)
+#have to append a first row before the for loop 
+      #somehow find the average of changes in profit
+      #averag =sum of the list / len the monthly changes
+      #the algorithm is correct but something is not working in the math, but this is closest I can do
+      average_change_profits = (sum(monthly_changes) / 85)
+
+      #Find the max and min change in profits 
+      max_increase_profits = max(monthly_changes)
+      max_decrease_profits = min(monthly_changes)
+      
+      #the date that the biggest dropped and raise happened
+      largest_date = date[monthly_changes.index(max_increase_profits)]
+      smallest_date = date[monthly_changes.index(max_decrease_profits)]
+
+    final_presentation = (
+    f"----------------------------\n"
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {len(date)}\n"
+    f"Total Revenue: ${sum(profit)}\n"
+    f"Average Revenue Change: {average_change_profits}\n"
+    f"Greatest Increase in Revenue: {(largest_date)} and ${(max_increase_profits)}\n"
+    f"Greatest Decrease in Revenue: : {(smallest_date)} and ${(max_decrease_profits)}\n"
+    f"----------------------------\n")
+    
+    print(final_presentation)
+
+#look at the end video three but also youtube because we only learned how to export csv and not text files
+output_path = os.path.join("Resources", "textfile.txt")
+
+
+with open(output_path, "w", newline='') as text_file:
+    text_file.write(final_presentation)
--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -1,1 +1,79 @@
-print(f"Hiiiii")
+#check video 2 of python to figure out where to start
+import os
+import csv
+import pandas as pd
+
+election_data_csv = os.path.join("Resources", "election_data.csv")
+
+# Objective 3: Create the lists to store data. 
+
+votes = []
+candidates = []
+list_canidates = {} #dictionary, but also attach 0/int to be able to count
+countervote_canidates = {"Khan": 0, "Correy": 0, "O'Tooley": 0, "Li": 0}
+#print(f'hi')
+
+
+
+# Open the CSV using the set path PyBankcsv
+
+with open(election_data_csv, encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+
+    # Read the header row first (skip this part if there is no header)
+    csv_header = next(csv_file)
+    #print(f"Header: {csv_header}")
+
+    for row in csv_reader:
+        #append to make lists
+        candidates.append(row[2])
+        votes.append(row)
+        
+        #next put the votes to each candidate
+    for vote_per_canidate in candidates:
+        if vote_per_canidate == "Khan":
+            countervote_canidates["Khan"] = countervote_canidates["Khan"] + 1
+        elif vote_per_canidate == "Li":
+            countervote_canidates["Li"] = countervote_canidates["Li"] + 1
+        elif vote_per_canidate == "O'Tooley":
+            countervote_canidates["O'Tooley"] = countervote_canidates["O'Tooley"] + 1
+        
+        elif vote_per_canidate == "Correy":
+            countervote_canidates["Correy"] = countervote_canidates["Correy"] + 1
+        
+        #this how you turn them to integers, so math functions work, make a seperate variable that is an int, tho revisit 
+        votes_for_khan = int(countervote_canidates["Khan"])
+        votes_for_Li = int(countervote_canidates["Li"])
+        votes_for_Correy = int(countervote_canidates["Correy"])
+        votes_for_OTooley = int(countervote_canidates["O'Tooley"])
+
+        total_votes = votes_for_khan + votes_for_Li + votes_for_Correy + votes_for_OTooley
+        #now do the percent portion
+        percent_khan = (votes_for_khan / total_votes) * 100
+        percent_Li = (votes_for_Li / total_votes) * 100
+        percent_OTooley = (votes_for_OTooley / total_votes) * 100
+        percent_Correy = (votes_for_Correy / total_votes) * 100
+    
+    #print(f'{total_votes}')
+
+    #just some it up to one variable so it's easy tp print and export
+    final_presentation = (
+    f"----------------------------\n"
+    f"Total votes: {total_votes}\n"
+    f"----------------------------\n"
+    f"Khan: {('{:.2f}'.format(percent_khan))}% ({votes_for_khan})\n"
+    f"Correy: {('{:.2f}'.format(percent_Correy))}% ({votes_for_Correy})\n"
+    f"Li: {('{:.2f}'.format(percent_Li))}% ({votes_for_Li}) \n"
+    f"O'Tooley: {('{:.2f}'.format(percent_OTooley))}% ({votes_for_OTooley})\n"
+    f"----------------------------\n"
+    f"Winner: Khan\n"
+    f"----------------------------\n")
+    
+    print(final_presentation)
+#look at the end video three but also youtube because we only learned how to export csv and not text files
+output_path = os.path.join("Resources", "textfile.txt")
+
+
+with open(output_path, "w", newline='') as text_file:
+    text_file.write(final_presentation)
+
